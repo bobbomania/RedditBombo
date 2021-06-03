2e662e8c5e681e203742504e9b648daa5af3d49c
Bobbomania <gabritro03@gmail.com>
2021-06-03 23:14:39 +0100

    folder path name in a file and language index fixed for all machines

--- src/scraper.py
+++ src/scraper.py
@@ -9,10 +9,17 @@ class subredditFinder:
     def __init__(self,subredditName,minCommentNb,minUpvotes, avgLetters):
 
         self.subreddit_NAME = subredditName
+
+        #the minimum number of comments in a subreddit
         self.min_comment_Nb = minCommentNb
+        
+        #the minimum amount of upvotes for a subreddit
         self.minUpvotes = minUpvotes
+
+        #the rate of speech of the tts
         self.RATE = 150
 
+        #the final list with the comments and the submission and its body content
         self.final_list = []
         self.final_script = ""
 
@@ -25,7 +32,13 @@ class subredditFinder:
         total_time = 0
 
         #the path of the folder of the reddit Bombo
-        folderPath = '\\'.join(os.path.dirname(os.path.abspath(__file__)).split('\\')[0:-1])
+        data_setup = open('setup.txt','r').readlines()
+
+        #folder path from data setup file
+        folderPath = data_setup[0]
+
+        #the language index from pyttsx3
+        langIndex = data_setup[1]
 
         #script for the tts
         self.final_script = ""
@@ -33,7 +46,7 @@ class subredditFinder:
         #insitialising the tts engine
         tts_engine = pyttsx3.init()
         tts_engine.setProperty('rate', self.RATE)
-        tts_engine.setProperty('voice', tts_engine.getProperty('voices')[2].id)
+        tts_engine.setProperty('voice', tts_engine.getProperty('voices')[langIndex].id)
 
         #getting the slurs in string format from the file
         slurs = [word[:-1] for word in open(folderPath + "\\utilities\\meanies.txt","r").readlines()]
@@ -93,14 +106,13 @@ class subredditFinder:
                 #removes adjacent repeated letters and \n
                 commentBundleBody += ''.join(c[0] for c in itertools.groupby(e.body.strip('\n')))
                 
-            self.final_script += commentBundleBody
+            self.final_script += commentBundleBody + ".........."
             total_time += (len(commentBundleBody)/self.AVG_Letters)/self.RATE
 
             #if the length of the video is more than 10min
             if total_time > 10:
                 break
                 
-            #to do adding spacing and punctuation to tts
             #find slurs and remove any repeated letters for tts
 
         #checking for swear words
@@ -111,7 +123,11 @@ class subredditFinder:
         ##        final_script = final_script.replace(slur,' '*len(slur))
         ##        
         print(self.final_script)
+        print(f'{folderPath}/output/tts/{self.subreddit_NAME} {len(os.listdir( folderPath + "/output/tts/" ))}.mp3')
+        tts_engine.save_to_file(self.final_script, f'{folderPath}/output/tts/{self.subreddit_NAME}{len(os.listdir( folderPath + "/output/tts/" ))}.mp3')
+        tts_engine.runAndWait()
 
+        return self.final_list
 
 askReddit = subredditFinder("AskReddit",40,1_000,4.7)
 askReddit.getTTS()
